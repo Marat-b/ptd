7d7e4e1128b7a72b1418f0c095fb07feeeda6314
marat-b <buran17@mail.ru>
2022-07-15 13:06:48 +0500

    torchscript actions

---
+++ utils/torchscript_save.py
@@ -0,0 +1,98 @@
+import argparse
+from multiprocessing import freeze_support
+
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.data import build_detection_test_loader
+from detectron2.data.datasets import register_coco_instances
+from detectron2.export import Caffe2Tracer, TracingAdapter, add_export_config, dump_torchscript_IR
+from detectron2.config import get_cfg
+from detectron2 import model_zoo
+from detectron2.modeling import GeneralizedRCNN, build_model
+import cv2
+import numpy as np
+import torch
+
+
+def main(args):
+    # weights_path = '../weights/potato_model_best_202205311000.pth'
+    weights_path = args.weights_path
+    test_coco_file_path = args.test_coco_file_path
+    test_images_path = args.test_images_path
+    output_path = args.output_path
+
+    register_coco_instances(
+        "potato_dataset_test", {},
+        test_coco_file_path,
+        test_images_path
+    )
+
+    img = cv2.imread('./images/000000000.jpg')
+
+    cfg = get_cfg()
+    # cfg = add_export_config(cfg)
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"))
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 10
+    cfg.DATASETS.TEST = ('potato_dataset_test',)
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.8
+    cfg.MODEL.WEIGHTS = weights_path
+    cfg.MODEL.DEVICE = 'cpu'
+    model = build_model(cfg)
+    DetectionCheckpointer(model).resume_or_load(cfg.MODEL.WEIGHTS)
+    model.eval()
+
+    # height, width = img.shape[:2]
+    image = torch.as_tensor(img.astype("float32").transpose(2, 0, 1))
+    # image = torch.randint(255, size=(3, 512, 512)).to(torch.float32)
+    print(image)
+    # inputs = {"image": image, "height": height, "width": width}
+    inputs = [{"image": image}]  # remove other unused keys
+    if isinstance(model, GeneralizedRCNN):
+        print('inference is Not None')
+
+        def inference(model, inputs):
+            # use do_postprocess=False so it returns ROI mask
+            inst = model.inference(inputs, do_postprocess=False)[0]
+            return [{"instances": inst}]
+    else:
+        print('inference is None')
+        inference = None  # assume that we just call the model directly
+    traceable_model = TracingAdapter(model, inputs, inference)
+    ################ torchscript ###################################################
+    ts_model = torch.jit.trace(traceable_model, (image,))
+    torch.jit.save(ts_model, output_path)
+    # dump_torchscript_IR(ts_model, '../weights/ts')
+
+
+if __name__ == '__main__':
+    freeze_support()
+    parser = argparse.ArgumentParser(description="Trainer Composition")
+    parser.add_argument(
+        "--test_coco_dir",
+        type=str,
+        dest="test_coco_file_path",
+        required=True,
+        help="Path of json file of COCO format"
+    )
+    parser.add_argument(
+        "--test_images_dir",
+        type=str,
+        dest="test_images_path",
+        required=True,
+        help=""
+    )
+    parser.add_argument(
+        "--weights",
+        type=str,
+        dest="weights_path",
+        required=True,
+        help=""
+    )
+    parser.add_argument(
+        "--output",
+        type=str,
+        dest="output_path",
+        required=True,
+        help=""
+    )
+    p_args = parser.parse_args()
+    main(p_args)
---
+++ utils/ts_detection.py
@@ -0,0 +1,111 @@
+import torchvision  # need to put first
+import cv2
+import torch
+import numpy as np
+
+
+class TorchscriptDetection:
+    def __init__(self, path_inference, use_cuda=True):
+        self.device = 'cuda' if use_cuda else 'cpu'
+        self.model = torch.jit.load(path_inference)
+        self.model.to(self.device)
+
+    def detect(self, image):
+        with torch.no_grad():
+            out = self.model(
+                     torch.as_tensor(image.astype('float32').transpose(2, 0, 1))
+            )
+        boxes = out[0].numpy()
+        classes = out[1].numpy()
+        scores = out[3].numpy()
+        # pr_masks = self._do_mask(out, image)
+        pr_masks = out[2].numpy()
+        # pr_masks = out.pred_masks.cpu().numpy()
+        # pr_masks = pr_masks.astype(np.uint8)
+        # pr_masks[pr_masks > 0] = 255
+        bbox_xcycwh, cls_conf, cls_ids, masks = [], [], [], []
+
+        for (box, _class, score, pr_mask) in zip(boxes, classes, scores, pr_masks):
+            # print(box)
+            x0, y0, x1, y1 = box
+            bbox_xcycwh.append([(x1 + x0) / 2, (y1 + y0) / 2, (x1 - x0), (y1 - y0)])
+            cls_conf.append(score)
+            cls_ids.append(_class)
+            masks.append(pr_mask)
+            length = self._get_width_pixel_ts(pr_mask, box)
+            # print(pr_mask)
+            print(pr_mask.dtype, pr_mask.shape)
+            new_mask = pr_mask.transpose((1, 2, 0))
+            new_mask[new_mask > 0.9] = 1.0
+            new_mask = new_mask.astype('uint8') * 255
+            new_mask = cv2.resize(new_mask, (length, length))
+            print(new_mask.dtype, new_mask.shape)
+            cv2.imshow('new_mask', new_mask)
+            cv2.waitKey(0)
+
+        return np.array(bbox_xcycwh, dtype=np.float64), np.array(cls_conf), np.array(cls_ids), np.array(masks)
+
+    # def _do_mask(self, out, image):
+    #     N = len(out[2])
+    #     chunks = torch.chunk(torch.arange(N, device='cpu'), N)
+    #     img_masks = torch.zeros(
+    #         N, image.shape[0], image.shape[1], device='cpu', dtype=torch.bool
+    #     )
+    #     # masks = out.pred_masks[:, 0, :, :]  # shape was [#instances, 1, 28, 28] --> [#instances, 28, 28]
+    #     for inds in chunks:
+    #         masks_chunk, spatial_inds = _do_paste_mask(
+    #             out[2][inds, :, :, :], out[0].tensor[inds], image.shape[0], image.shape[1],
+    #             skip_empty=True
+    #         )
+    #
+    #         masks_chunk = (masks_chunk >= 0.5).to(dtype=torch.bool)
+    #         img_masks[(inds,) + spatial_inds] = masks_chunk
+    #         # print(img_masks.shape)
+    #     # print(np.array(np.uint8(img_masks)*255))
+    #     return np.array(np.uint8(img_masks)*255)
+    def _get_width_pixel_ts(self, image_mask, box):
+        """
+        Get biggest side of object (width) in pixels
+        Parameters:
+        ----------
+            image_mask:  np.array -  image mask (black-white)
+        Returns:
+        -------
+            width: int - width in pixel
+        """
+        def calculate_distance(point_a, point_b):
+            dist = np.sqrt((point_a[0] - point_b[0]) ** 2 + (point_a[1] - point_b[1]) ** 2)
+            return dist
+        width = box[2] - box[0]
+        height = box[3] - box[1]
+        # print(f'width={width}, height={height}')
+        box_len = width if width > height else height
+        mask = image_mask[0]  # .astype('uint8')
+        mask[mask > 0.9] = 1.0
+        mask = mask.astype('uint8')
+        f = np.argwhere(mask > 0)
+        f_max = np.argmax(f, axis=0)
+        bottom = f[f_max[0]]
+        right = f[f_max[1]]
+        f_min = np.argmin(f, axis=0)
+        top = np.array([f[f_min[0]][0], bottom[1]])
+        left = np.array([right[0], f[f_min[1]][1]])
+        width = calculate_distance(left, right)
+        height = calculate_distance(top, bottom)
+        length = width if width > height else height
+        return int((box_len * length) / 28)
+
+
+if __name__ == '__main__':
+    tsd = TorchscriptDetection('../weights/potato_best20220715.ts', use_cuda=False)
+    img = cv2.imread('../images/400000000.jpg')
+    pred_boxes, scores, pred_classes, pred_masks = tsd.detect(img)
+    print(f'pred_boxes={pred_boxes}')
+    print(f'scores={scores}')
+    print(f'pred_classes={pred_classes}')
+    if len(pred_masks) > 0:
+        print(f'pred_masks={pred_masks[0].shape}')
+    # im = tsd.detect2(img)
+    # print(im)
+    # cv2.imshow('im', im)
+    # cv2.waitKey(1000)

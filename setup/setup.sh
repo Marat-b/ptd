ca0d1df1916968ba0d504380955da0b324e916c0
marat-b <buran17@mail.ru>
2022-10-26 19:57:14 +0500

    setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,8 +1,8 @@
-sudo apt-get install python3.8-dev
-sudo apt-get install unrar
-sudo apt-get install libjpeg-dev zlib1g-dev
-sudo apt-get install gcc libpq-dev
-sudo apt-get install libgeos-dev
-sudo apt-get install python-setuptools
-sudo apt-get install python3.8-venv
-sudo apt-get install python3-pip
+apt-get install python3.8-dev
+apt-get install unrar
+apt-get install libjpeg-dev zlib1g-dev
+apt-get install gcc libpq-dev
+apt-get install libgeos-dev
+apt-get install python-setuptools
+apt-get install python3.8-venv
+apt-get install python3-pip

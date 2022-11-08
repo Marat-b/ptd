37f15ace6967b1c4b0708292169f86e17f140224
marat-b <buran17@mail.ru>
2022-11-08 12:39:37 +0500

    mapper 20221108

--- potato_trainer.py
+++ potato_trainer.py
@@ -60,8 +60,8 @@ class PotatoTrainer:
         self.cfg.DATASETS.TRAIN = ('train_instances',)
         self.cfg.DATASETS.TEST = ('validate_instances',)
         self.cfg.DATALOADER.NUM_WORKERS = 2
-        # self.cfg.MODEL.RPN.BATCH_SIZE_PER_IMAGE = 256
-        self.cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 64 # default 512
+        self.cfg.MODEL.RPN.BATCH_SIZE_PER_IMAGE = 512
+        # self.cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 64 # default 512
         self.cfg.SOLVER.IMS_PER_BATCH = 2
 
         self.cfg.SOLVER.GAMMA = 0.5
